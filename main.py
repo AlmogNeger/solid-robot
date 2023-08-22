7a43a8cc1fc5d1f05e053b77c4f323742f0979e9
Almog Neger <ALmog.Neger@opsys-sens.com>
2023-08-22 17:04:20 +0300

    somthing new

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 print("what is your name?")
 print("my name is almog")
-print("how so you feel?")
+print("how so ygit ou feel?")
 print("i am master printer")
+#this comment is for me-new branch
+print("something new!")
